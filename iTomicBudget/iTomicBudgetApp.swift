ce182f41ae0bfb3f2ee22820e100c55023659a72
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-30 13:28:29 -0600

    Initial Commit

---
+++ iTomicBudget/iTomicBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTomicBudgetApp.swift
+//  iTomicBudget
+//
+//  Created by J. DeWeese on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct iTomicBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
